fcc6b0d804d090829cd53f30054ef33f57000a62
Andre Carter <carstar2486@gmail.com>
2020-04-27 17:02:33 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -9,8 +9,8 @@ def fourth_challenge
         {:name => "Benvolio",
         :age => "17",
         :attitude => "worried"},
-        {:name => "Mercutio"
-        :age => "18"
+        {:name => "Mercutio",
+        :age => "18",
         :attitude => "hot-headed"}
       ]
    }, 
@@ -20,11 +20,11 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        {:name => "Steven"
-        :age => "30"
+        {:name => "Steven",
+        :age => "30",
         :attitude => "confused"},
-        {:name => "Nurse"
-        :age => "44"
+        {:name => "Nurse",
+        :age => "44",
         :attitude => "worried"}
       ]
    }
